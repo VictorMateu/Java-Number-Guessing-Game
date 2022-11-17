cd0ffff327e641481e327e8066d02d98d99c0f94
Victor Mateu <victormateulopez@gmail.com>
2022-11-17 15:12:58 +0100

    first commit

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,4 +1,16 @@
 import java.util.Scanner;
 
 public class NumberGuessingGame {
+
+    public static void guessingNumberGame(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+
+    }
+
+
+
+    public static void main(){
+        guessingNumberGame();
+    }
 }
